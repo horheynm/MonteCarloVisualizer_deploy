7054369d5c0f1d2c497aa423126978d68e36bcba
George <george.ohashi@gmail.com>
2021-05-15 16:56:06 -0400

    added hashrouter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import MonteCarlo from './MonteCarlo/MonteCarlo'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Homepage from './Homepage/Homepage'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { HashRouter as Router, Route } from 'react-router-dom'
 
 function App() {
   return (
